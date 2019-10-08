a5381e6e44d9769a1a6866ae2b1eb74b8b0bd8c9
alexanderkrum <MR-XF559_yolo>
2019-10-08 21:50:48 +1100

    Use lint staged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react'
 import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <h1>Hello</h1>
+  </div>
+)
 
 export default App
